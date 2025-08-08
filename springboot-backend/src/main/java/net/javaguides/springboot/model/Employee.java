7503f9be14bc33090cedccd11de7ebe16cdd1a17
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-09 00:42:02 +0530

    Initial commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String firstName;
+}
