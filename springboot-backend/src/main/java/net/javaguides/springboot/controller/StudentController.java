7867dad81c47acf046d516f9bc82a4db45e82bbd
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-10 01:43:39 +0530

    finalize stdController

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,9 +1,23 @@
 package net.javaguides.springboot.controller;
 
+import lombok.RequiredArgsConstructor;
+import net.javaguides.springboot.model.Student;
+import net.javaguides.springboot.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
 }
