32c79e636316ab2376498991246043b967cbc6cb
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-10 00:27:09 +0530

    complete student attributes

--- springboot-backend/src/main/java/net/javaguides/springboot/model/Student.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Student.java
@@ -16,10 +16,18 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "STUDENT_ID")
     private long studentId;
 
+    @Column(name = "FIRST_NAME",length = 20)
     private String firstName;
+
+    @Column(name = "LAST_NAME",length = 20)
     private String lastName;
+
+    @Column(name = "AGE")
     private int age;
+
+    @Column(name = "ADRESS")
     private String adress;
 }
