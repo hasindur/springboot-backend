72bb9b036d6c78b96440e0df40d75645f29787fd
Hasindu Ranasinghe <152050830+hasindur@users.noreply.github.com>
2025-08-10 01:43:53 +0530

    finalize stdService

--- springboot-backend/src/main/java/net/javaguides/springboot/service/StudentService.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/service/StudentService.java
@@ -1,4 +1,25 @@
 package net.javaguides.springboot.service;
 
+import lombok.RequiredArgsConstructor;
+import net.javaguides.springboot.dao.StudentDao;
+import net.javaguides.springboot.model.Student;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class StudentService {
+
+    private final StudentDao studentDao;
+
+    public ResponseEntity<Student> saveStudent(Student student) {
+
+        student = studentDao.save(student);
+
+        if (student.getStudentId() == null){
+            throw new RuntimeException("Student Save Fail");
+        }else {
+            return ResponseEntity.ok(student);
+        }
+    }
 }
